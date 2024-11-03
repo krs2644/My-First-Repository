2432c53f69f47b568334b30b093c4cc4fb15f061
krs2644 <krsarvesh426@gmail.com>
2024-11-03 23:29:49 +0530

    first commit

---
+++ 2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MALLOC(p,n,type)\
+p =(type*)malloc(n*sizeof(type));\
+if (p == NULL){\
+	printf("Insufficient memory\n");\
+	exit(0);\
+}
+
+void main()
+{
+	int *p1, *p2;
+	int sum;
+	MALLOC (p1,1,int);
+	MALLOC (p2,1,int);
+	printf("%d\n",p1);
+	printf("%d\n",p2);
+	*p1 = 10;
+	*p2 = 20;
+	sum= *p1 + *p2;
+	printf("\n%d + %d = %d\n",*p1,*p2,sum);
+}
